111cdced8689404cb6446bff4fa4712dfcf7ce56
sanjit singh <ss3448@scarletmail.rutgers.edu>
2024-12-19 18:47:57 -0500

    got vuzix HUD sdk sucessfully integrated

---
+++ You_Tube_WIFI_APP/app/src/main/java/com/example/you_tube_wifi_app/VuzixTest.java
@@ -0,0 +1,16 @@
+package com.example.you_tube_wifi_app;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.vuzix.hud.actionmenu.ActionMenuActivity;
+
+public class VuzixTest extends ActionMenuActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
---
+++ You_Tube_WIFI_APP/app/src/main/java/com/example/you_tube_wifi_app/dynamic_theme_recevier.java
@@ -0,0 +1,15 @@
+package com.example.you_tube_wifi_app;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class dynamic_theme_recevier extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
